65898b039ab212e17e3274a79e4ef314173a2a47
masood ahmadi <masood.ahmadi553@gmail.com>
2025-10-19 14:42:09 +0300

    backend creatation

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
-// api/index.js
-const app = require("../app");
+const express = require("express");
 const serverless = require("serverless-http");
+const cors = require("cors");
+
+const users = require("../routes/users");  // adjust relative path
+const posts = require("../routes/posts");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Mount routes
+app.use("/api/users", users);
+app.use("/api/posts", posts);
 
 module.exports = serverless(app);
