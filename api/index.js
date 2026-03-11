74c579905e2718b27bdc3eaef86a622a9dbff248
masood ahmadi <masoodahmadi553@gmail.com>
2026-03-11 21:29:10 +0200

    prepare for vercel deployment

--- api/index.js
+++ api/index.js
@@ -4,13 +4,18 @@ const cors = require("cors");
 const users = require("../routes/users")
 
 const posts = require("../routes/posts");
+const { connect } = require("mongoose");
 
 
 const app = express();
+connect();
 app.use(cors());
 app.use(express.json());
 
 // Mount your routes
+app.get("/", (req, res) => {
+    res.json({ message: "API working on Vercel" });
+});
 app.use("/api/users", users);
 app.use("/api/posts", posts);
 
