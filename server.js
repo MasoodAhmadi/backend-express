f0b6a4022c8e50fc2ff055cfa625215818b1155c
masood ahmadi <masoodahmadi553@gmail.com>
2026-04-08 17:08:34 +0300

    updated serverjs file

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const cors = require("cors");
+
 const users = require("./routes/users");
 const connectDB = require('./lib/db');
+const PORT = process.env.PORT || 5000;
 
 connectDB();
 
 app.use(express.json());
-–
+app.use(cors());
+
+app.use("/api/users", users);
+// app.use("/api/posts", posts);
+
+
+app.use("/", (req, res) => {
+    res.send("Hello World!");
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
